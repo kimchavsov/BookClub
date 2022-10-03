9da901454f0245c02d06ad58ff2c4ea268fb01a7
Kimchav Sov <kimchavsov@gmail.com>
2022-10-04 00:07:17 +0800

    add session

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-const express = require('express')
-const engine = require('ejs-mate')
-const methodOverride = require('method-override')
-const mongoose = require('mongoose')
-const path = require('path')
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
+const express = require('express');
+const engine = require('ejs-mate');
+const LocalStrategy = require('passport-local');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const MongoStore = require('connect-mongo');
+const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
 
-const Book = require('./models/book')
-const User = require('./models/user')
+const Book = require('./models/book');
+const User = require('./models/user');
 
-const booksRouter = require('./routes/book')
-const userRoute = require('./routes/user')
+const booksRouter = require('./routes/book');
+const userRoute = require('./routes/user');
 
 mongoose.connect('mongodb://localhost:27017/bookshelf')
   .then(() => {
@@ -21,19 +23,34 @@ mongoose.connect('mongodb://localhost:27017/bookshelf')
     console.log('Connection Error')
   })
 
-const app = express()
+const app = express();
+
+const store = MongoStore.create({
+  mongoUrl: 'mongodb://localhost:27017/bookshelf',
+  secret: 'testing',
+  touchAfter: 24 * 3600
+})
 
 
-app.engine('ejs', engine)
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(methodOverride('_method'))
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+app.use(session({
+  name: 'mysession',
+  secret: 'thisissecret',
+  saveUninitialized: false,
+  resave: false,
+  store: store
+}))
 
 
-passport.use(new LocalStrategy(User.authenticate()))
+passport.use(new LocalStrategy(User.authenticate()));
 
 // for passport session support
 passport.serializeUser(User.serializeUser());
@@ -41,12 +58,12 @@ passport.deserializeUser(User.deserializeUser());
 
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home');
 })
 
 app.use('/books', booksRouter);
-app.use('/', userRoute)
+app.use('/', userRoute);
 
 app.listen(3000, () => {
-  console.log("Listening to port 3000")
+  console.log("Listening to port 3000");
 })
