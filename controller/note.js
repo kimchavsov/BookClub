c405f80254551b2558a2968ada00f40c62e1496a
kimchavsov <kimchavsov@gmail.com>
2022-11-09 00:46:44 +0800

    populate dropdown to show books and add logic to create note on note page

--- controller/note.js
+++ controller/note.js
@@ -9,14 +9,15 @@ module.exports.renderNotes = async (req, res) => {
 }
 
 module.exports.renderNewNote = async (req, res) => {
-  res.render("notes/new");
+  const books = await Book.find({owner: {$eq: req.user._id }});
+  res.render("notes/new", { books });
 }
 
 module.exports.createNote = async (req, res) => {
   // Handle note create from book
   if (req.params.id) {
     const book = await Book.findById(req.params.id);
-    note = new Note(req.body.note);
+    const note = new Note(req.body.note);
     note.book = req.params.id;
     book.notes.push(note);
     await note.save();
@@ -24,8 +25,9 @@ module.exports.createNote = async (req, res) => {
     return res.redirect(`/books/${req.params.id}`);
   } else {
     // TODO: Handle create note from note creation
-    const book = await Book.findById(req.body.note.bookID);
-    note = new Note(req.body.note);;
+    const book = await Book.findById(req.body.note.book);
+    const note = new Note({title: req.body.note.title, content: req.body.note.content});
+    note.book = req.body.note.book;
     book.notes.push(note);
     await note.save();
     await book.save();
