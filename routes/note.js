1114a095554eef4eff3a8590839ea06279332bc8
kimchavsov <kimchavsov@gmail.com>
2022-11-16 01:20:13 +0800

    fix ui on view note page

--- routes/note.js
+++ routes/note.js
@@ -3,9 +3,6 @@ const note = require("../controller/note");
 
 const router = express.Router();
 
-// router.route('/')
-//   .post(console.log("hello"))
-
 
 router.route('/')
   .get(note.renderNotes)
